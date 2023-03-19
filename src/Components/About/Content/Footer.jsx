8a2b411c6c1713c197b9b675ce414b633395c2e5
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2023-03-19 16:36:29 +0530

    update

--- src/Components/About/Content/Footer.jsx
+++ src/Components/About/Content/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="p-3 mt-3 text-center w-full text-accent ">
-      Made with &#10084; in india
+      Made with &#10084; by Nasyx Rakeeb in india
     </footer>
   );
 }
